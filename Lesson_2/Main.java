fe53a4430a07c9c7266d31f861cb7cfb99638a1b
annacarkova043-coder <annacarkova043@gmail.com>
2026-02-01 05:45:29 +0300

    Добавлен метод checkSumSign

--- Lesson_2/Main.java
+++ Lesson_2/Main.java
@@ -1,11 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
     
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+    
+    public static void checkSumSign() {
+        int a = 15;
+        int b = -7;
+        int sum = a + b;
+        
+        if (sum >= 0) {
+            System.out.println("Sum is positive");
+        } else {
+            System.out.println("Sum is negative");
+        }
     }
 }
